2faad8fc32bb3b7c391e7635e0ee8fd97a572b7f
TMania808 <83692753+TMania808@users.noreply.github.com>
2022-10-28 14:16:02 -0700

    Implemented best practices and stretch goals

--- index.js
+++ index.js
@@ -5,22 +5,33 @@ const lengthText = document.getElementById("length-text")
 const volumeText = document.getElementById("volume-text")
 const massText = document.getElementById("mass-text")
 
-convertBtn.addEventListener("click", function () {
+convert()
+
+function convert() {
     let num = Number(userInput.value)
 
     if (!userInput.value) {
-        lengthText.innerHTML = "-- meters = -- feet | -- feet = -- meters"
-        volumeText.innerHTML = "-- liters = -- gallons | -- gallons = -- liters"
-        massText.innerHTML = "-- kilograms = -- pounds | -- pounds = -- kilograms"
-    } else {
-        lengthText.innerHTML = `${num} meters = ${(num * 3.28084).toFixed(3)} feet | 
-                                ${num} feet = ${(num / 3.28084).toFixed(3)} meters`
-
-        volumeText.innerHTML = `${num} liters = ${(num * 0.264172).toFixed(3)} gallons | 
-                                ${num} gallons = ${(num / 0.264172).toFixed(3)} liters`
-
-        massText.innerHTML = `${num} kilograms = ${(num * 2.20462).toFixed(3)} pounds | 
-                            ${num} pounds = ${(num / 2.20462).toFixed(3)} kilograms`
+        num = 1
     }
-})
+        
+    lengthText.innerHTML = `${num} meters = ${(num * 3.28084).toFixed(3)} feet | 
+                            ${num} feet = ${(num / 3.28084).toFixed(3)} meters`
+
+    volumeText.innerHTML = `${num} liters = ${(num * 0.264172).toFixed(3)} gallons | 
+                            ${num} gallons = ${(num / 0.264172).toFixed(3)} liters`
+
+    massText.innerHTML = `${num} kilograms = ${(num * 2.20462).toFixed(3)} pounds | 
+                        ${num} pounds = ${(num / 2.20462).toFixed(3)} kilograms`
+    
+}
+
+convertBtn.addEventListener("click", convert)
+
+// Stretch Goal Code//
+
+userInput.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        convert();
+    }
+});
 
